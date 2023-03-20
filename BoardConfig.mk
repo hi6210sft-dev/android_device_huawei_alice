c694859521922fb4532490ef577a36599fd80645
Jiyong Park <jiyong@google.com>
2023-03-20 21:27:17 +0100

    alice: set TARGET_FLATTEN_APEX=true
    APEXes are flattened for hi6210sft. Kernel is too old to support APEXes
    without performance regression.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,9 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
+# Apexes
+TARGET_FLATTEN_APEX := true
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := hi6210sft
 
