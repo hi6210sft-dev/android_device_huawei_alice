873d693875c2773e5a53f2a95b7238380005ebaa
A2L5E0X1 <alexed25@protonmail.com>
2023-03-22 11:04:00 +0100

    alice: Get rid of unsupported encoding format(s)

--- extract-files.sh
+++ extract-files.sh
@@ -55,6 +55,16 @@ if [ -z "$SRC" ]; then
     SRC=adb
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        system/etc/camera/*)
+	    sed -i 's/gb2312/iso-8859-1/g' "${2}"
+	    sed -i 's/GB2312/iso-8859-1/g' "${2}"
+	    sed -i 's/xmlversion/xml version/g' "${2}"
+            ;;
+    esac
+}
+
 # Initialize the helper
 setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
 
