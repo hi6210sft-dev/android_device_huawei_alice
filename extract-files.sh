ec96b9bf2b49ae764183c2a8b2ee0074728cb8b9
R0rt1z2 <rogerortizleal@gmail.com>
2023-03-22 20:16:41 +0100

    alice: proprietary-files: Get rid of `system/` prefix
    * This was some leftover.

--- extract-files.sh
+++ extract-files.sh
@@ -57,22 +57,22 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-        system/etc/camera/*)
+        etc/camera/*)
 	    sed -i 's/gb2312/iso-8859-1/g' "${2}"
 	    sed -i 's/GB2312/iso-8859-1/g' "${2}"
 	    sed -i 's/xmlversion/xml version/g' "${2}"
             ;;
-        system/bin/chargemonitor)
+        bin/chargemonitor)
             sed -i 's/\([Uu][Cc][Nn][Vv]_[A-Za-z_]*\)_55/\1_63/g' "${2}"
             ;;
-        system/bin/glgps)
+        bin/glgps)
             sed -i 's/\([Uu][Cc][Nn][Vv]_[A-Za-z_]*\)_55/\1_63/g' "${2}"
             sed -i "s/SSLv3_client_method/SSLv23_method\x00\x00\x00\x00\x00\x00/" "${2}"
             ;;
-        system/lib*/hw/audio.primary.hi6210sft.so)
+        lib*/hw/audio.primary.hi6210sft.so)
             sed -i 's/\([Uu][Cc][Nn][Vv]_[A-Za-z_]*\)_55/\1_63/g' "${2}"
             ;;
-        system/lib*/libril.so)
+        lib*/libril.so)
             sed -i 's/\([Uu][Cc][Nn][Vv]_[A-Za-z_]*\)_55/\1_63/g' "${2}"
             ;;
     esac
