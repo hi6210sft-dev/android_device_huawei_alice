dbd75504a317391831515199c693708a0df6f52f
Meticulus <theonejohnnyd@gmail.com>
2023-03-24 10:39:29 +0100

    alice: Import opensource libhwcomposer

--- device.mk
+++ device.mk
@@ -86,7 +86,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    android.hardware.renderscript@1.0-impl
+    android.hardware.renderscript@1.0-impl \
+    hwcomposer.hi6210sft
 
 # Keymaster
 PRODUCT_PACKAGES += \
