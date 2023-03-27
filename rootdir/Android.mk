6a65e00d6a4258660399f7070c789c3db1e3f5b6
R0rt1z2 <rogerortizleal@gmail.com>
2023-03-27 12:52:01 +0200

    alice: rootdir: Get rid of Huawei-specific addition(s)

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -56,14 +56,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.manufacture.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.manufacture.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.tee.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
