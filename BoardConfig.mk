0c86eaca6515472d004a51d1dd3ad7f985a840fa
R0rt1z2 <rogerortizleal@gmail.com>
2023-03-28 13:13:08 +0200

    alice: wifi: configure firmware paths

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -145,6 +145,10 @@ BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA   := "/system/vendor/firmware/fw_bcm4343.bin"
+WIFI_DRIVER_FW_PATH_AP    := "/system/vendor/firmware/fw_bcm4343s_apsta_hw.bin"
+WIFI_DRIVER_FW_PATH_P2P   := "/system/vendor/firmware/fw_bcm4343s_p2p_hw.bin"
 
 # SELinux
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
