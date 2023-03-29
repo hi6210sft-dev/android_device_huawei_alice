347bc054619c2665a08f73d32678fbf327d0fed2
Meticulus <theonejohnnyd@gmail.com>
2023-03-29 22:12:08 +0200

    alice: shim missing exif symbols for camera
    * dlopen failed: cannot locate symbol "exif_entry_gps_initialize" referenced by "/system/lib/libcamera_core.so"...

--- libshims/libshim_camera.cpp
+++ libshims/libshim_camera.cpp
@@ -1,4 +1,9 @@
 #include <SkStream.h>
+#include <libexif/exif-entry.h>
+
+extern "C" void exif_entry_gps_initialize(ExifEntry * e, ExifTag tag) {
+    exif_entry_initialize(e,tag);
+}
 
 extern "C" void _ZN22SkDynamicMemoryWStreamC1Ev(SkDynamicMemoryWStream* self) {
     new(self) SkDynamicMemoryWStream();
