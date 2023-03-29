51514f9b3b09449ef11c0141d85c65ee5fff5983
Eduardo Alonso <edu@error404software.com>
2023-03-29 23:06:33 +0200

    alice: libshims: Provide dummy __srget for camera

--- libshims/libshim_camera.cpp
+++ libshims/libshim_camera.cpp
@@ -1,5 +1,11 @@
+#include <stdio.h>
+
 #include <SkStream.h>
 #include <libexif/exif-entry.h>
+
+extern "C" int __srget(FILE *fp) {
+    return (EOF);
+}
 
 extern "C" void exif_entry_gps_initialize(ExifEntry * e, ExifTag tag) {
     exif_entry_initialize(e,tag);
