f893224f8142e1bf0cc18e9d380afd368c044b74
R0rt1z2 <rogerortizleal@gmail.com>
2023-03-31 00:11:40 +0200

    alice: Fork default hardware camera implementation

--- device.mk
+++ device.mk
@@ -79,6 +79,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl-legacy \
+    camera.device@1.0-impl-alice \
     libcamera_parameters_ext_alice
 
 PRODUCT_PACKAGES += \
