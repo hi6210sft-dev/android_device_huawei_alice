6fbb1e0a9a56f620ad63e633b270c115cf2332b8
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-04-01 00:21:01 +0200

    alice: Build sensors HIDL

--- device.mk
+++ device.mk
@@ -210,6 +210,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/configs/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service \
+    libsensorndkbridge
+
 # Shim libraries
 PRODUCT_PACKAGES += \
     libshim_audio \
