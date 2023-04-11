dda4dd87d1afb7019bf584b468a560c5dc0256dd
R0rt1z2 <rogerortizleal@gmail.com>
2023-04-11 17:45:25 +0200

    alice: nvme: Unmap the previous data_buffer if mapping failed

--- include/nvme.h
+++ include/nvme.h
@@ -21,7 +21,6 @@ char *nve_read(int fd, const char *nve_name) {
     uint32_t block_size = 0, offset = 0;
     unsigned char *data_buffer = nullptr, *ptr = nullptr;
     char* nve_data = static_cast<char*>(malloc(NVE_NV_DATA_SIZE));
-    //unsigned char *nve_data = malloc(NVE_NV_DATA_SIZE);
 
     data_buffer = (unsigned char *)mmap(NULL, 1, PROT_READ, MAP_SHARED, fd, 0);
     if (data_buffer == MAP_FAILED) return nullptr;
@@ -34,8 +33,13 @@ char *nve_read(int fd, const char *nve_name) {
     munmap(data_buffer, 1);
     while (block_size >= 0) {
         data_buffer = (unsigned char *)mmap(NULL, 0x00020000, PROT_READ, MAP_SHARED, fd, block_size);
-        if (data_buffer == MAP_FAILED) return nullptr;
-
+        if (data_buffer == MAP_FAILED) {
+            // Unmap the previous data_buffer if mapping failed
+            if (block_size > 0) {
+                munmap(data_buffer, 0x00020000);
+            }
+            return nullptr;
+        }
         ptr = static_cast<unsigned char *>(memmem(data_buffer, 0x00020000, nve_name, strlen(nve_name)));
         if (ptr) {
             offset = (ptr - data_buffer) + NV_NAME_LENGTH + 12;
