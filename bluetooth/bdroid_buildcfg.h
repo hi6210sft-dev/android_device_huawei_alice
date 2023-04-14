fd7f7a5544b053a995f6ee59c2a4fadda0de61d9
R0rt1z2 <rogerortizleal@gmail.com>
2023-04-14 11:52:31 +0200

    alice: bluetooth: Load `BTM_DEF_LOCAL_NAME` dynamically
    Co-authored-by: luca020400 <luca.stefani.ge1@gmail.com>
    Co-authored-by: Kyle Harrison <khwebmail@gmail.com>
    Co-authored-by: dianlujitao <dianlujitao@lineageos.org>
    Co-authored-by: R0rt1z2 <me@r0rt1z2.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -2,7 +2,32 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Huawei P8 Lite 2015"
+#include <stdint.h>
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+int property_get(const char *key, char *value, const char *default_value);
+#ifdef __cplusplus
+}
+#endif
+
+inline const char* BtmGetDefaultName()
+{
+	char device[92];
+	property_get("ro.product.model", device, "");
+
+        if (strstr(device, "ALE") != NULL) {
+            return "Huawei P8 Lite 2015";
+        } else if (strstr(device, "CAM") != NULL) {
+            return "Huawei Y6II";
+        }
+
+	return "Huawei";
+}
+
+#define BTM_DEF_LOCAL_NAME   BtmGetDefaultName()
 #define BTM_BYPASS_EXTRA_ACL_SETUP TRUE
 
 #endif
